d4111922dc41fb4f517b973f74e1a53a79473f42
SYED AQEEL BIN ZAINAL ABIDIN ALJUNIED 2202748F <2202748F@student.tp.edu.sg>
2023-11-26 01:22:55 +0800

    Added database and default data

---
+++ CarRentalManagement/Server/Configurations/Entities/ColourSeedConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using CarRentalManagement.Shared.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using Humanizer;
+
+namespace CarRentalManagement.Server.Configurations.Entities
+{
+    public class ColourSeedConfiguration : IEntityTypeConfiguration<Colour>
+    {
+        public void Configure(EntityTypeBuilder<Colour> builder)
+        {
+            builder.HasData(
+                new Colour
+                {
+                    Id = 1,
+                    Name = "Black",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Colour
+                {
+                    Id = 2,
+                    Name = "Blue",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+                );
+        }
+    }
+}
